52f224a0f31ac93aef00a3de04c6306158a4aed3
katW20 <k.teranm@uniandes.edu.co>
2026-08-24 12:17:34 -0500

    release lab

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -3,3 +3,23 @@ def new_list(size):
     "elements": [],
     "size":0,
     }
+    
+    return new_list
+
+def add_first(my_list, element):
+    my_list["elements"].insert(0, element)
+    my_list["size"] += 1
+    return my_list
+
+def add_last(my_list, element):
+    my_list["elements"].append(element)
+    my_list["size"] += 1
+    return my_list
+
+def size(my_list):
+
+    return my_list["size"]
+
+def first_element(my_list):
+    return my_list["elements"][0]
+    
